8e9a3b325bb8416c7f5e0070d9319b99d1c524e2
william-alcantara <wil-alcantara@hotmail.com>
2020-01-21 23:30:18 -0300

    codigo padrao

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,11 +1,19 @@
 package apllication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+				
 		System.out.println("Bom dia");
-
+		
+		sc.close();
 	}
 
 }
